1f2c39b4805641b7ffeb1b9b4c9eea4c7f3c4e4a
Brian Hamlin <bhamlin92@gmail.com>
2024-05-31 22:23:27 -0400

    add handler for login from email

--- src/app/login/login.container.ts
+++ src/app/login/login.container.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
-import { getAuth, sendSignInLinkToEmail } from '@angular/fire/auth';
+import { Component, Optional } from '@angular/core';
+import {
+  Auth,
+  isSignInWithEmailLink,
+  sendSignInLinkToEmail,
+  signInWithEmailLink,
+} from '@angular/fire/auth';
 
 @Component({
   selector: 'app-login',
@@ -9,22 +14,45 @@ import { getAuth, sendSignInLinkToEmail } from '@angular/fire/auth';
 export class LoginContainer {
   email: string = '';
 
-  constructor() {}
+  constructor(@Optional() private auth: Auth) {}
+
+  ngOnInit(): void {
+    this.checkSignInLink();
+  }
 
   async sendSignInLink() {
-    const auth = getAuth();
     const actionCodeSettings = {
       url: 'http://localhost:4200', // Update with your app's URL
       handleCodeInApp: true,
     };
 
-    try {
-      await sendSignInLinkToEmail(auth, this.email, actionCodeSettings);
-      window.localStorage.setItem('emailForSignIn', this.email);
-      alert('A sign-in link has been sent to your email.');
-    } catch (error) {
-      console.error('Error sending email:', error);
-      alert('There was an error sending the email.');
+    sendSignInLinkToEmail(this.auth, this.email, actionCodeSettings)
+      .then(() => {
+        alert(`Sign in link was sent to ${this.email}`);
+        window.localStorage.setItem('emailForSignIn', this.email);
+        // ...
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }
+
+  async checkSignInLink() {
+    if (isSignInWithEmailLink(this.auth, window.location.href)) {
+      let email = window.localStorage.getItem('emailForSignIn');
+      console.log('email from localstorage: ', email);
+      if (!email) {
+        email = window.prompt('Please provide your email for confirmation');
+        return;
+      }
+      signInWithEmailLink(this.auth, email, window.location.href)
+        .then((result) => {
+          window.localStorage.removeItem('emailForSignIn');
+          alert('Successfully signed in!');
+        })
+        .catch((error) => {
+          alert(error);
+        });
     }
   }
 }
