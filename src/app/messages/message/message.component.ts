a4d02b28c9c6d8d54ddf06c3a55f4b31b8d20d0f
Brian Hamlin <bhamlin92@gmail.com>
2024-06-01 13:08:54 -0400

    add basic countdown/reveal feature

--- src/app/messages/message/message.component.ts
+++ src/app/messages/message/message.component.ts
@@ -12,8 +12,12 @@ export class MessageComponent {
   message: Message = {
     title: 'my note',
     content: 'my content',
-    delayInMinutes: 1
+    delayInMinutes: .2
   };
+
+  timerFinished: boolean = false;
+  countdownMilliseconds: number = 0;
+  timerDisplay: string = '';
 
   constructor(private route: ActivatedRoute, private router: Router) { }
 
@@ -22,6 +26,31 @@ export class MessageComponent {
       const messageId = params['id'];
       console.log('messageId: ', messageId)
     });
+
+    this.countdownMilliseconds = this.message.delayInMinutes * 60 * 1000;
+    this.startCountdown();
+  }
+
+  startCountdown(): void {
+    // Update timer display every second
+    const timerInterval = setInterval(() => {
+      // Calculate minutes and seconds
+      const minutes = Math.floor(this.countdownMilliseconds / 60000);
+      const seconds = ((this.countdownMilliseconds % 60000) / 1000).toFixed(0);
+
+      // Display the timer
+      this.timerDisplay = `${minutes}:${(Number(seconds) < 10 ? '0' : '')}${seconds}`;
+
+      // Decrease remaining time
+      this.countdownMilliseconds -= 1000;
+
+      // Check if countdown is finished
+      if (this.countdownMilliseconds < 0) {
+        clearInterval(timerInterval); // Stop the timer
+        this.timerDisplay = '0:00'; // Display 0:00
+        this.timerFinished = true;
+      }
+    }, 1000);
   }
 
   goBack(): void {
