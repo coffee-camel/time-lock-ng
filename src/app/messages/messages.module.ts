5091379ec8ac3dae9211c97db87fcc0a976a3d9f
Brian Hamlin <bhamlin92@gmail.com>
2024-06-02 14:36:18 -0400

    remove old message component

--- src/app/messages/messages.module.ts
+++ src/app/messages/messages.module.ts
@@ -3,7 +3,6 @@ import { SharedModule } from '../_@shared/shared.module';
 import { MessagesContainer } from './messages.container';
 import { RouterModule } from '@angular/router';
 import { MESSAGES_ROUTES } from './messages.routes';
-import { MessageComponent } from './message/message.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { SidebarItemComponent } from './sidebar-item/sidebar-item.component';
 import { MessageDisplayComponent } from './message-display/message-display.component';
@@ -12,7 +11,6 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 @NgModule({
   declarations: [
     MessagesContainer,
-    MessageComponent,
     SidebarComponent,
     SidebarItemComponent,
     MessageDisplayComponent,
