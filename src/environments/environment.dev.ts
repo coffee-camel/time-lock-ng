eb764296d45e9c024bf8af3fa1554d74c4689263
Brian Hamlin <bhamlin92@gmail.com>
2024-06-04 16:41:27 -0400

    update prod config

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    projectId: 'timelock-b6fa1',
-    appId: '1:772583398496:web:ac9d6c3fc192f8be3df705',
-    storageBucket: 'timelock-b6fa1.appspot.com',
     apiKey: 'AIzaSyARYrmgK3wssim_BNCfjbVgn1MLLKcDUwE',
     authDomain: 'timelock-b6fa1.firebaseapp.com',
+    projectId: 'timelock-b6fa1',
+    storageBucket: 'timelock-b6fa1.appspot.com',
     messagingSenderId: '772583398496',
     measurementId: 'G-072LET2D1W',
-  }
+    appId: '1:772583398496:web:ac9d6c3fc192f8be3df705',
+  },
 };
