72f1a27c33287fda2b3fe333633e3977bb78da65
Brian Hamlin <bhamlin92@gmail.com>
2024-06-13 17:26:17 -0400

    add web worker and timer in tab title

--- src/app/_@features/notes/components/note-display/note-display.component.ts
+++ src/app/_@features/notes/components/note-display/note-display.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output, OnDestroy, inject } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { EncryptionService } from '_@core/encryption.service';
 import { Note } from '_@core/notesFirebase.service';
 
@@ -16,61 +17,60 @@ export class NoteDisplayComponent implements OnDestroy {
   public timerDisplay: string = '';
 
   private _note: Note | null = null;
-  private countdownMilliseconds: number = 0;
-  private timerInterval: any;
+  private worker: Worker;
+
+  constructor(private titleService: Title) {
+    this.worker = new Worker(new URL('./timer.worker', import.meta.url));
+    this.worker.onmessage = ({ data }) => this.handleWorkerMessage(data);
+  }
+
+  ngOnDestroy(): void {
+    this.worker.terminate();
+    this.setTitle('TimeLock');
+  }
 
   @Input()
   set note(value: Note | null) {
-    this._note = value;
-    this.isTimerFinished = false;
-    this.timerFinished.emit(false);
+    if (value !== this._note) {
+      this._note = value;
+      this.isTimerFinished = false;
+      this.timerFinished.emit(false);
 
-    if (this._note) {
-      this.countdownMilliseconds = this._note.delayInMinutes * 60 * 1000;
-      this.startCountdown();
+      if (this._note) {
+        const countdownMilliseconds = this._note.delayInMinutes * 60 * 1000;
+        this.worker.postMessage({ action: 'start', countdownMilliseconds });
+      } else {
+        this.worker.postMessage({ action: 'stop' });
+      }
     }
   }
   get note(): Note | null {
     return this._note;
   }
 
-  startCountdown(): void {
-    this.clearInterval();
-
-    this.timerInterval = setInterval(() => {
-      if (this._note) {
-        const minutes = Math.floor(this.countdownMilliseconds / 60000);
-        const seconds = ((this.countdownMilliseconds % 60000) / 1000).toFixed(0);
-
-        this.timerDisplay = `${minutes}:${Number(seconds) < 10 ? '0' : ''}${seconds}`;
-        this.countdownMilliseconds -= 1000;
-
-        if (this.countdownMilliseconds < 0) {
-          this.clearInterval();
-          this.timerDisplay = '';
-          this.isTimerFinished = true;
-          this.timerFinished.emit(true);
-        }
-      } else {
-        this.clearInterval();
-      }
-    }, 1000);
+  private handleWorkerMessage(data: any): void {
+    if (data.action === 'tick') {
+      const minutes = Math.floor(data.countdownMilliseconds / 60000);
+      const seconds = ((data.countdownMilliseconds % 60000) / 1000).toFixed(0);
+      this.timerDisplay = `${minutes}:${Number(seconds) < 10 ? '0' : ''}${seconds}`;
+      this.setTitle(`${this.timerDisplay} | TimeLock`);
+    } else if (data.action === 'finished') {
+      this.timerDisplay = '';
+      this.isTimerFinished = true;
+      this.timerFinished.emit(true);
+      this.setTitle('TimeLock');
+    }
   }
 
-  clearInterval(): void {
-    if (this.timerInterval) {
-      clearInterval(this.timerInterval);
-      this.timerInterval = null;
-    }
+  private setTitle(title: string): void {
+    this.titleService.setTitle(title);
   }
 
   onCancel(): void {
     this._note = null;
     this.timerDisplay = '';
     this.cancel.emit();
-  }
-
-  ngOnDestroy(): void {
-    this.clearInterval();
+    this.worker.postMessage({ action: 'stop' }); // Send stop signal to Web Worker
+    this.setTitle('TimeLock');
   }
 }
